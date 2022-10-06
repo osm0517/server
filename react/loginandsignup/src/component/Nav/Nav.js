b9722f89ac325156875ff5d98c79b495f9dd929c
unknown <sung min oh>
2022-10-06 20:39:15 +0900

    login and signup design finish

--- react/loginandsignup/src/component/Nav/Nav.js
+++ react/loginandsignup/src/component/Nav/Nav.js
@@ -6,10 +6,10 @@ function Nav() {
 
     return(
         <div className="nav">
-            <span ><a className="list" href="/view/top">상의</a></span>
-            <span ><a className="list" href="/view/bottom">하의</a></span>
-            <span ><a className="list" href="/view/accessories">악세사리</a></span>
-            <span ><a className="list" href="/view/beauty">뷰티</a></span>
+            <span ><a className="list" href="/view/top">TOP</a></span>
+            <span ><a className="list" href="/view/bottom">BOTTOM</a></span>
+            <span ><a className="list" href="/view/accessories">ACC</a></span>
+            <span ><a className="list" href="/view/beauty">BEAUTY</a></span>
             <div className="nav-button">
                 <div />
                 <div />
